22abe60150233ca66e5c43bcf3e5ef299c5a9671
johntrandall <john@johnrandall.com>
2014-07-12 15:07:10 -0400

    updated tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: persons
+#
+#  id                     :integer          not null, primary key
+#  company_id             :integer
+#  type                   :string(255)
+#  name                   :string(255)
+#  address                :string(255)
+#  email_1                :string(255)
+#  email_2                :string(255)
+#  email_3                :string(255)
+#  home_phone             :string(255)
+#  office_phone           :string(255)
+#  mobile_phone           :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#
+
 require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
