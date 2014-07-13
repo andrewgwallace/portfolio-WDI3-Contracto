db425d095b806d48cad6ad8770b2aefe00c80323
Andrew Wallace <andrew.g.wallace@gmail.com>
2014-07-12 22:01:15 -0400

    Added forms for job list, jobs, entries, and sidebars.  Updated balsamiq mockups.

--- app/assets/javascripts/backbone/backbone_application_loader.js
+++ app/assets/javascripts/backbone/backbone_application_loader.js
@@ -5,45 +5,27 @@ var app = app || { Models: {}, Collections: {}, Views: {} };
 
 app.initialize = function(){
   console.log("running app.initialize (backbone_app.js)");
-  
-
-  // //Setup clock
-  // kid = new app.Models.Kid({name:'Max'});
-  // kidClockView = new app.Views.KidView({
-  //   model: kid, 
-  //   el: $('#kid-clock-column')
-  // })
-  // kidClockView.render()
-
 
   //Setup jobs
   jobsCollection = new app.Collections.JobsCollection();
-
 
   var jobListView = new app.Views.JobListView({
     collection: jobsCollection,
     el: $('#jobs-table-body')
   });
-  
+
   jobsCollection.fetch();
+},
 
+// Setup entries
+entriesCollection = new app.Collections.EntriesCollection();
 
-}
+var entryListView = new app.Views.EntryListView({
+  collection: entriesCollection,
+  el: $('#entries-table-body')
+});
 
-
-
-
-$(function(){
-  console.log("DOM ready from backbone_app_loader.js");
-  
-  // var oldSync = Backbone.sync;
-  // Backbone.sync = function(method, model, options){
-  //   options.beforeSend = function(xhr){
-  //   xhr.setRequestHeader('X-CSRFToken', CSRF_TOKEN);
-  //   };
-  //   return oldSync(method, model, options);
-  // };
+  entriesCollection.fetch();
 
   app.initialize();
-  // testView = new app.Views.ChildcareView({model: new app.Models.Childcare({name: 'hello there'})})
 });
