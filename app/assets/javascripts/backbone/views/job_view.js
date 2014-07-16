2f29704663751fe91109390cb0e598a23fbf4b7d
johntrandall <john@johnrandall.com>
2014-07-16 18:59:07 -0400

    Jobs Index: fixed the target / currentTarget bug. Added icons that dynamically switch

--- app/assets/javascripts/backbone/views/job_view.js
+++ app/assets/javascripts/backbone/views/job_view.js
@@ -32,23 +32,43 @@ app.Views.JobView = Backbone.View.extend({
       return this;
     },
     tagName: "tr",
-    className: 'job',
+    className: 'job table_row',
 
 
   // EVENTS AND DATA ACTIONS
 
     events: {
       'click [data-action]' : 'processDataAction',
+      'hover [data-action]' : 'testHover',
     },
 
     processDataAction: function(event){
       console.log('running processDataAction');
-      console.log('action is ' +event.target.dataset.action);
+
+      // console.log('THIS is'+this);
+      // console.log(this)
+      // console.log('---------------------')
+      
+      // console.log('EVENT.target is'+event.target);
+      // console.log(event.target);
+      // console.log('---------------------')
+      
+      // console.log('EVENT.currentTarget is'+event.target);
+      // console.log(event.currentTarget);
+      // console.log('---------------------')
+      
+      console.log('action is ' +event.currentTarget.dataset.action);
+
       // console.log(event.target.dataset.action);
       // console.log(this.model.attributes.id);
-      this.model['hello']();
-      this.model[event.target.dataset.action]();
-
+      // this.model['hello']();
+      this.model[event.currentTarget.dataset.action]();
     },
+
+    testHover: function(event){
+      console.log('running testhover')
+      // this.addClass('test-blue')
+      event.target.dataset.addClass('test-blue');
+    }
 
 });
