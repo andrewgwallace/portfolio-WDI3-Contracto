393309890afa342b1bcc338e931146fa35f8e2f3
johntrandall <john@johnrandall.com>
2014-07-17 02:50:17 -0400

    mostly functioning nav bar

--- app/assets/javascripts/navigation_init.js
+++ app/assets/javascripts/navigation_init.js
@@ -1,20 +1,34 @@
+console.log('reading navigation_init');
+
 $(function(){
+    console.log('DOM ready from navigation_init');
 
-    $('#header-customers-link').on('click', function(){
-      console.log('customers link')
-        hide('.focus-area');
-        show('#customers-focus');
+
+    $( "#dataTable tbody tr" ).on( "click", function() {
+          alert( $( this ).text() );
+        });
+
+    $('#header-jobs-link').on("click", function(){
+      console.log('jobs link');
+        $('.focus-area').hide();
+        $('#jobs-focus').show();
     });
 
-    $('#header-sub-contractors-link').on('click', function(){
-      console.log('subcontractors link')
-        hide('.focus-area');
-        show('#subcontractors-focus');
+    $('#header-customers-link').on("click", function(){
+      console.log('customers link');
+        $('.focus-area').hide();
+        $('#customers-focus').show();
     });
 
-    $('#header-employees-link').on('click', function(){
-      console.log('employees link')
-        hide('.focus-area');
-        show('#employees-focus');
+    $('#header-sub-contractors-link').on("click", function(){
+      console.log('subcontractors link');
+        $('.focus-area').hide();
+        $('#subcontractors-focus').show();
+    });
+
+    $('#header-employees-link').on("click", function(){
+      console.log('employees link');
+        $('.focus-area').hide();
+        $('#employees-focus').show();
     });
 });
