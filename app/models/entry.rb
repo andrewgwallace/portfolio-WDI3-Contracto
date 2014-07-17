fbc38e4822065a9f1a3a4bf93bad922f24f1dcb1
johntrandall <john@johnrandall.com>
2014-07-17 01:42:10 -0400

    added scopes to entry and job

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,18 +2,27 @@
 #
 # Table name: entries
 #
-#  id                                 :integer          not null, primary key
-#  job_id                             :integer
-#  text_blob                          :text
-#  read                               :boolean
-#  auto_generated_from_incoming_email :boolean
-#  read_status                        :boolean
-#  created_at                         :datetime
-#  updated_at                         :datetime
+#  id                                   :integer          not null, primary key
+#  job_id                               :integer
+#  text_blob                            :text
+#  read                                 :boolean
+#  auto_generated_from_incoming_email   :boolean
+#  read_status                          :boolean
+#  visable_to_job_client_status         :boolean
+#  visable_to_job_subcontractors_status :boolean
+#  created_at                           :datetime
+#  updated_at                           :datetime
 #
 
 class Entry < ActiveRecord::Base
 
+  scope :visable_to_job_client_status_scope, -> { where(visable_to_job_client_status: true) }    #'open is a reserved word'
+  scope :hidden_from_job_client_status_scope, -> { where(visable_to_job_client_status: false) }    #'open is a reserved word'
+
+  scope :visable_to_job_subscontractors_status_scope, -> { where(visable_to_job_subscontractors_status_scope: true) }
+  scope :hidden_from_job_subscontractors_status_scope, -> { where(visable_to_job_subscontractors_status_scope: false) }
+
+  
   belongs_to :job
   has_many :attachments
     has_many :photos
